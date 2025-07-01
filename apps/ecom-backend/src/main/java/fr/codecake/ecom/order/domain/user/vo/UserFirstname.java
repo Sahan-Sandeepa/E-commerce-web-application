3f290c00bb71b9aebea3946fb37c9fde83f58e7c
Sahan_SJ <sahansandeepa0003@gmail.com>
2025-07-01 18:17:08 +0530

    Adding user authentication & authorization related implementations
    Co-authored-by: Sahan-Sandeepa <sahansandeepa0003@gmail.com>
    
    Co-authored-by: Sahan Jayawardhana <it20611156@my.sliit.lk>

---
+++ apps/ecom-backend/src/main/java/fr/codecake/ecom/order/domain/user/vo/UserFirstname.java
@@ -0,0 +1,10 @@
+package main.java.fr.codecake.ecom.order.domain.user.vo;
+
+import fr.codecake.ecom.shared.error.domain.Assert;
+
+public record UserFirstname(String value) {
+
+  public UserFirstname {
+    Assert.field("value", value).maxLength(255);
+  }
+}
