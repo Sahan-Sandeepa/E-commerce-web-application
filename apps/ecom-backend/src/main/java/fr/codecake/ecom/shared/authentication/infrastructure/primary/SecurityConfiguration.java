6a2cea4ad599b7e4296efe85c680275d7066ead1
Sahan_SJ <sahansandeepa0003@gmail.com>
2025-07-03 02:52:49 +0530

    Refact: refactoring almost all the errors and conneting with the DB
    Co-authored-by: Sahan-Sandeepa <sahansandeepa0003@gmail.com>
    
    Co-authored-by: Sahan Jayawardhana <it20611156@my.sliit.lk>

--- apps/ecom-backend/src/main/java/fr/codecake/ecom/shared/authentication/infrastructure/primary/SecurityConfiguration.java
+++ apps/ecom-backend/src/main/java/fr/codecake/ecom/shared/authentication/infrastructure/primary/SecurityConfiguration.java
@@ -18,10 +18,10 @@ public class SecurityConfiguration {
   public SecurityFilterChain configure(HttpSecurity http) throws Exception {
     http.authorizeHttpRequests(authorize ->
         authorize
-          .requestMatchers(HttpMethod.GET, "api/categories").permitAll()
-          .requestMatchers(HttpMethod.GET, "api/products-shop/**").permitAll()
-          .requestMatchers(HttpMethod.GET, "api/orders/get-cart-details").permitAll()
-          .requestMatchers(HttpMethod.POST, "api/orders/webhook").permitAll()
+          .requestMatchers(HttpMethod.GET, "/api/categories").permitAll()
+          .requestMatchers(HttpMethod.GET, "/api/products-shop/**").permitAll()
+          .requestMatchers(HttpMethod.GET, "/api/orders/get-cart-details").permitAll()
+          .requestMatchers(HttpMethod.POST, "/api/orders/webhook").permitAll()
           .requestMatchers("/api/**").authenticated())
       .csrf(AbstractHttpConfigurer::disable)
       .oauth2ResourceServer(
