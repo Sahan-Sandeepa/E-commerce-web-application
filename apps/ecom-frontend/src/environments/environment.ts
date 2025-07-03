b4c0d939e5d4df9590dc5546bca554e469c30521
Sahan_SJ <sahansandeepa0003@gmail.com>
2025-07-03 20:53:59 +0530

    Setting up the frontend to communicate with the backend
    Co-authored-by: Sahan-Sandeepa <sahansandeepa0003@gmail.com>
    
    Co-authored-by: Sahan Jayawardhana <it20611156@my.sliit.lk>

---
+++ apps/ecom-frontend/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  kinde: {
+    authority: 'https://codecake.kinde.com',
+    redirectUrl: 'http://localhost:4200',
+    postLogoutRedirectUri: 'http://localhost:4200',
+    clientId: 'f870e5ab3b524965b6c9b9c907c230c1',
+    audience: 'https://codecake.kinde.com/api',
+  },
+  apiUrl: 'http://localhost:8080/api',
+  stripePublishableKey:
+    'pk_test_51PqP6gFxB2dnnKdDhiB1cqZyvA0X1Si7ggeltnoO0OxXVRVMr35Lk8Z1UpsgbzjyawMNIpGlaB91S72uhbGvPpyJ00H5Zy8La7',
+};
