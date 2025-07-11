9a558c69b1d1c1f6c0675cc048f69b96eaf99bc2
Sahan_SJ <sahansandeepa0003@gmail.com>
2025-07-12 03:42:17 +0530

    Bug: fixing the auth related issues
    Co-authored-by: Sahan-Sandeepa <sahansandeepa0003@gmail.com>
    
    Co-authored-by: Sahan Jayawardhana <it20611156@my.sliit.lk>

--- apps/ecom-frontend/src/app/auth/callback.component.ts
+++ apps/ecom-frontend/src/app/auth/callback.component.ts
@@ -1,29 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { OidcSecurityService } from 'angular-auth-oidc-client';
+import { take } from 'rxjs';
 
 @Component({
-    selector: 'app-callback',
-    template: `<p>Authenticating...</p>`,
+  selector: 'app-callback',
+  template: `<p>Authenticating...</p>`,
 })
 export class CallbackComponent implements OnInit {
-    constructor(
-        private oidcSecurityService: OidcSecurityService,
-        private router: Router
-    ) { }
+  constructor(
+    private oidcSecurityService: OidcSecurityService,
+    private router: Router
+  ) {}
 
-    ngOnInit(): void {
-        this.oidcSecurityService.checkAuth().subscribe({
-            next: (auth) => {
-                if (auth.isAuthenticated) {
-                    this.router.navigate(['/']);
-                } else {
-                    this.router.navigate(['/login']);
-                }
-            },
-            error: () => {
-                this.router.navigate(['/login']);
-            },
-        });
-    }
+  ngOnInit(): void {
+    this.oidcSecurityService
+      .checkAuth()
+      .pipe(take(1))
+      .subscribe({
+        next: (auth) => {
+          if (auth.isAuthenticated) {
+            this.router.navigate(['/'], { replaceUrl: true });
+          } else {
+            this.router.navigate(['/login']);
+          }
+        },
+        error: () => {
+          this.router.navigate(['/login']);
+        },
+      });
+  }
 }
