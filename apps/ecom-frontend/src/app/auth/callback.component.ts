ccf1f3f69f0648a0e3c791820582ce8dae139838
Sahan_SJ <sahansandeepa0003@gmail.com>
2025-07-19 23:11:10 +0530

    bug: fixing the global CSS related issues & changing the UI for best interactions
    Co-authored-by: Sahan-Sandeepa <sahansandeepa0003@gmail.com>
    
    Co-authored-by: Sahan Jayawardhana <it20611156@my.sliit.lk>

--- apps/ecom-frontend/src/app/auth/callback.component.ts
+++ apps/ecom-frontend/src/app/auth/callback.component.ts
@@ -5,13 +5,18 @@ import { take } from 'rxjs';
 
 @Component({
   selector: 'app-callback',
-  template: `<p>Authenticating...</p>`,
+  template: `<div class="flex h-screen w-screen items-center justify-center bg-gray-50 dark:bg-gray-900">
+  <div class="flex flex-col items-center space-y-4">
+    <div class="animate-spin rounded-full h-12 w-12 border-t-4 border-primary border-solid"></div>
+    <p class="text-gray-700 dark:text-gray-300 text-lg font-medium">Authenticating, please wait...</p>
+  </div>
+  </div>`,
 })
 export class CallbackComponent implements OnInit {
   constructor(
     private oidcSecurityService: OidcSecurityService,
     private router: Router
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     this.oidcSecurityService
