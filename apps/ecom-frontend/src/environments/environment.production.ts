c0916bc42b665afcae49712f0f2faba40d204c28
Sahan Sandeepa <122716935+Sahan-Sandeepa@users.noreply.github.com>
2025-07-25 18:40:59 +0530

    Update environment.production.ts

--- apps/ecom-frontend/src/environments/environment.production.ts
+++ apps/ecom-frontend/src/environments/environment.production.ts
@@ -1,13 +1,1 @@
-export const environment = {
-  kinde: {
-    authority: 'https://ecomcodecake.kinde.com',
-    postLogoutRedirectUri: 'http://localhost:4200',
-    clientId: 'b1848662d51c48af8788b63542d4a8ec',
-    redirectUrl: 'http://localhost:4200/callback',
-    audience: 'http://localhost:8080/api'
-
-  },
-  apiUrl: 'http://localhost:8080/api',
-  stripePublishableKey:
-    'pk_test_51Rn0kBRoFHcyuYurJBVxvg8UxoNvFYK2LfPp73aHTs4huwBU2sVZaM9pN10cmthi2lYgz0HKfVe44ghw9ZayCmwg00Un6YzWsX',
-};
+export const environment = {};
